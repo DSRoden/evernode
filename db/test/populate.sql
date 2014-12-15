28aa54251cbf446a4c538dd15b242a66f5aed783
DSRoden <Daniel.S.Roden@gmail.com>
2014-12-15 14:08:40 -0600

    sql sequence fix

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,1 +1,7 @@
+insert into users (id, username, password, avatar, token) values ('a', 'b', 'c', 'd');
+insert into users (id, username, password, avatar, token) values ('a', 'b', 'c', 'd');
+insert into users (id, username, password, avatar, token) values ('a', 'b', 'c', 'd');
+
+delete from users;
+
 insert into users (id, username, password, avatar, token) values (1, 'bob', '$2a$08$iJFefo.q4FqkMGT./qx89.bctGaSxCmoe9MvrxL.9ZdZhGRME7tmm', 'a.png', 'tok');
