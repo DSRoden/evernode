a46b4c73cf36643ca381b8cb828a426e36fcb614
DSRoden <Daniel.S.Roden@gmail.com>
2014-12-15 11:58:27 -0600

    register test

---
+++ test/unit/user.js
@@ -0,0 +1,44 @@
+/* jshint expr:true */
+
+'use strict';
+
+var expect           = require('chai').expect,
+  cp                 = require('child_process'),
+  User               = require('../../server/models/user'),
+  helpers            = require('../helpers/helpers'),
+  Lab                = require('lab'),
+  lab                = exports.lab = Lab.script(),
+  describe           = lab.describe,
+  it                 = lab.it,
+  beforeEach         = lab.beforeEach,
+  db                 = helpers.getdb();
+  console.log(db);
+
+describe('User', function(){
+
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      done();
+    });
+  });
+
+  describe('constructor', function(){
+    it('should create a User object', function(done){
+    var user = new User({username: 'bob'});
+    expect(user).to.be.instanceof(User);
+    expect(user.username).to.equal('bob');
+    done();
+    });
+  });
+
+  describe('.register', function(){
+    it('should register a new User', function(done){
+      User.register({username: 'sam', password: '123', avatar: 'http://images.apple.com/global/elements/flags/16x16/usa_2x.png'}, function(err){
+        expect(err).to.be.null;
+        done();
+      });
+    });
+  });
+
+
+});
