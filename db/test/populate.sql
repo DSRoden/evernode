95d8569362cde40bc611a7b6c775b3fdb0731a3a
DSRoden <Daniel.S.Roden@gmail.com>
2014-12-15 15:53:21 -0600

    note test

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -4,4 +4,5 @@ insert into users (username, password, avatar, token) values ('a', 'b', 'c', 'd'
 
 delete from users;
 
+
 insert into users (id, username, password, avatar, token) values (1, 'bob', '$2a$08$iJFefo.q4FqkMGT./qx89.bctGaSxCmoe9MvrxL.9ZdZhGRME7tmm', 'a.png', 'tok');
